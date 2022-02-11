651b67e8f43867af4923642d5679219ff760a97e
Seth Wiggins <bswigg17@gmail.com>
2022-02-11 09:23:17 -0500

    Fix

--- flips.js
+++ flips.js
@@ -2,8 +2,13 @@ import { coinFlips } from './modules/coin.mjs';
 
 
 try {
-    const args = parseInt(process.argv.slice(2)[0].slice(9)); 
-    console.log(coinFlips(args));
+    const args = process.argv.slice(2)[0].slice(9); 
+    if (NaN(args)) {
+        throw new Error()
+    }
+    else {
+        console.log(coinFlips(parseInt(args))); 
+    }
 }
 catch (error) {
     console.log(coinFlips(1));
